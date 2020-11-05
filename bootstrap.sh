5006e5aefc971bda83243f890ce17f6264666d70
MarkWengSTR <bskin0330@gmail.com>
2020-11-05 23:16:35 +0800

    Modify bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,128 +2,142 @@
 #
 # bootstrap script, assumed to be run as root
 #
-# to used it as vagrant provision shell script, invoke with parameter 'vagrant'
-#
 
-source /etc/os-release
+set -e
 
-if [[ "$NAME" = "Ubuntu" && "$VERSION_ID" = "14.04" ]]; then
-  # update package list
-  sed -i 's/archive\.ubuntu\.com/ftp\.cuhk\.edu\.hk\/pub\/Linux/g' /etc/apt/sources.list
-  sed -i 's/security\.ubuntu\.com/ftp\.cuhk\.edu\.hk\/pub\/Linux/g' /etc/apt/sources.list
-  apt-get update
-
-  apt-get -y install python-software-properties
-
-  # setup proper locale
-  locale-gen en_HK.utf8 zh_TW.utf8
-  update-locale LC_ALL=en_HK.utf8 LANG=en_HK.utf8
-  export LANG=en_HK.UTF8
-
-  # install required packages
-  apt-get -y install build-essential git-core git-extras tig wget curl htop tmux \
-    rake exuberant-ctags vim-gtk silversearcher-ag zsh ntp
-  apt-get -y install nginx postgresql-9.3 postgresql-contrib-9.3 redis-server
-  apt-get -y install bison openssl libssl-dev libxslt1.1 libxslt1-dev \
-    libxml2 libxml2-dev libffi-dev libyaml-dev autoconf \
-    libc6-dev libreadline6-dev zlib1g zlib1g-dev \
-    ruby-dev libruby2.0 libsqlite3-dev libpq-dev
-  apt-get -y install pdftk poppler-utils openjdk-7-jdk
-
-elif [[ "$NAME" = "Ubuntu" && "$VERSION_ID" = "12.04" ]]; then
-  # update package list
-  sed -i 's/archive\.ubuntu\.com/ftp\.cuhk\.edu\.hk\/pub\/Linux/g' /etc/apt/sources.list
-  sed -i 's/security\.ubuntu\.com/ftp\.cuhk\.edu\.hk\/pub\/Linux/g' /etc/apt/sources.list
-  apt-get update
-
-  apt-get -y install python-software-properties
-
-  # setup proper locale
-  locale-gen en_HK.utf8
-  update-locale LC_ALL=en_HK.utf8 LANG=en_HK.utf8
-  export LANG=en_HK.UTF8
-
-  # get latest repository
-  add-apt-repository -y ppa:git-core/ppa
-  add-apt-repository -y ppa:fcwu-tw/ppa
-  add-apt-repository -y ppa:kalakris/tmux
-
-  # get latest postgres repository
-  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-  echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/postgresql.list
-
-  apt-get update
-
-  # install required packages
-  apt-get -y install build-essential git-core tig wget curl htop tmux \
-    rake exuberant-ctags vim-gtk silversearcher-ag zsh ntp
-  apt-get -y install nginx postgresql-9.3 postgresql-contrib-9.3 redis-server
-  apt-get -y install bison openssl libssl-dev libxslt1.1 libxslt1-dev \
-    libxml2 libxml2-dev libffi-dev libyaml-dev libxslt-dev autoconf \
-    libc6-dev libreadline6-dev zlib1g zlib1g-dev \
-    ruby-dev libopenssl-ruby libsqlite3-dev libpq-dev
-  apt-get -y install pdftk poppler-utils openjdk-7-jdk
-
-elif [[ "$NAME" = "ArchLinux" ]]; then
-  # update package list
-  pacman -Sy
-  pacman --noconfirm --needed -S base-devel git tig wget curl htop tmux \
-    ruby gvim ctags ack zsh the_silver_searcher
-  pacman --noconfirm --needed -S bison openssl libxslt libxml2 libyaml libffi \
-    autoconf readline zlib
-
+if [[ "$LANG" != "zh_TW.utf8" ]]; then
+  locale-gen zh_TW.utf8
+  update-locale LC_ALL=zh_TW.utf8 LANG=zh_TW.utf8
 fi
 
-#
-# install chruby
-#
-if [ ! -d ~/src/chruby ]; then
-  git clone git://github.com/postmodern/chruby.git ~/src/chruby
-  cd ~/src/chruby
-  make install
+timedatectl set-timezone Asia/Taipei
+
+apt update
+DEBIAN_FRONTEND=noninteractive apt -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" upgrade
+apt autoremove -y
+
+apt install -y curl htop silversearcher-ag poppler-utils tree unzip wget
+apt install -y autoconf build-essential default-jdk libssl1.0-dev libreadline-dev \
+               libxml2-dev libxslt1-dev zlib1g-dev libpq-dev libsqlite3-dev \
+              python-dev python-pip python3-dev python3-pip nginx
+# apt install -y postgresql postgresql-contrib redis-server
+
+# 2 new items need to install for ubuntu 18
+apt install -y fontconfig # fc-list & fc-cache
+apt install -y zip
+
+# install zsh
+apt install -y libncurses5-dev yodl
+git clone git://github.com/zsh-users/zsh.git /tmp/zsh --branch zsh-5.7.1 --depth 1
+cd /tmp/zsh
+./Util/preconfig
+./configure --with-tcsetpgrp
+make && make check && sudo make install
+
+# install git
+apt install -y libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl1.0-dev
+git clone git://github.com/git/git.git /tmp/git --branch v2.25.0 --depth 1
+cd /tmp/git
+make all prefix=/usr/local
+make install prefix=/usr/local
+
+# install tmux
+apt install -y pkg-config libevent-dev bison
+git clone git://github.com/tmux/tmux.git /tmp/tmux --branch 3.1 --depth 1
+cd /tmp/tmux
+sh autogen.sh
+./configure
+make && make install
+
+# install tig
+apt install -y libncursesw5-dev
+git clone git://github.com/jonas/tig.git /tmp/tig --branch tig-2.3.3 --depth 1
+cd /tmp/tig
+make ./configure
+./configure
+make prefix=/usr/local
+make install prefix=/usr/local
+
+# install vim
+apt install -y exuberant-ctags
+git clone git://github.com/vim/vim.git /tmp/vim --branch v8.1.0117 --depth 1
+cd /tmp/vim
+./configure --with-features=big --enable-fail-if-missing
+make && make install
+
+# install neovim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install -y neovim
+mkdir -p ~/.config/nvim
+wget https://github.com/MarkWengSTR/win_dotfiles/blob/master/init.vim ~/.config/nvim/init.vim
+
+if [[ -r /usr/local/bin/zsh ]]; then
+  chsh -s /usr/local/bin/zsh
 fi
 
-#
-# install ruby-install
-#
-if [ ! -d ~/src/ruby-install ]; then
-  git clone git://github.com/postmodern/ruby-install.git ~/src/ruby-install
-  cd ~/src/ruby-install
-  make install
+# install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt update && apt install -y yarn
+# remove outdated nodejs with yarn
+apt remove -y --purge nodejs
+
+# Install asdf
+asdf_dir=$HOME/.asdf
+cd $HOME
+
+if [ ! -d $asdf_dir  ]; then
+  echo "Installing asdf..."
+  apt install jq -y # for asdf java
+  git clone https://github.com/asdf-vm/asdf.git /home/vagrant/.asdf --branch v0.8.0
+
+  echo "asdf installation complete"
+else
+  echo "asdf already installed"
 fi
 
-#
-# install git-extras
-#
-if [ ! -d ~/src/git-extras ]; then
-  git clone git://github.com/tj/git-extras ~/src/git-extras
-  cd ~/src/git-extras
-  git checkout $(git describe --tags $(git rev-list --tags --max-count=1))
-  make install
-fi
+asdf plugin-add java    || true
+asdf plugin-add clojure || true
+asdf plugin-add rust    || true
+asdf plugin-add python  || true
+asdf plugin-add golang  || true
+asdf plugin-add ruby    || true
+asdf plugin-add nodejs  || true
 
-
-if [[ $1 == "vagrant" ]]; then
-  # set local timezone
-  echo 'Asia/Hong_Kong' | tee /etc/timezone
-  dpkg-reconfigure --frontend noninteractive tzdata
-
-  # install personalize development environment
-  su -l -c '[ ! -d ~/.dotfiles ] && git clone git://github.com/szetobo/dotfiles.git ~/.dotfiles; ~/.dotfiles/run.sh install' vagrant
-  chsh -s /bin/zsh vagrant
-
-  # install phantomjs, dependency of rails integration test
-  cd /tmp
-  wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
-  tar xvjf phantomjs-1.9.7-linux-x86_64.tar.bz2
-  mv phantomjs-1.9.7-linux-x86_64 /usr/local/share/phantomjs
-  ln -s /usr/local/share/phantomjs/bin/phantomjs /usr/local/bin/phantomjs
-
-  # install nodejs
-  cd /tmp
-  wget http://nodejs.org/dist/v0.10.28/node-v0.10.28-linux-x64.tar.gz
-  cd /usr/local && tar --strip-components 1 -xzf /tmp/node-v0.10.28-linux-x64.tar.gz
-fi
+asdf install
 
 exit 0
+# # install libreoffice
+# cd /tmp
+# wget -nv http://downloadarchive.documentfoundation.org/libreoffice/old/4.2.8.2/deb/x86_64/LibreOffice_4.2.8.2_Linux_x86-64_deb.tar.gz
+# tar xvf LibreOffice_4.2.8.2_Linux_x86-64_deb.tar.gz
+# cd LibreOffice_4.2.8.2_Linux_x86-64_deb/DEBS
+# dpkg -i *.deb
+# ln -sf `readlink /usr/bin/libreoffice4.2` /usr/local/bin/libreoffice
+
+# install phantomjs (skip this because takes very long time)
+# cd /tmp
+# wget -nv https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+# tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2
+# mv -f phantomjs-2.1.1-linux-x86_64 /usr/local/share/phantomjs-2.1.1
+# ln -sf /usr/local/share/phantomjs-2.1.1/bin/phantomjs /usr/local/bin/phantomjs
+
+# # allow vagrant login by password (for pair session)
+# if [[ -z $(grep 'PasswordAuthentication yes' /etc/ssh/sshd_config) ]]; then
+#   echo -e "\nMatch User vagrant\n  PasswordAuthentication yes" >> /etc/ssh/sshd_config
+#   sshd -t && service ssh reload
+# fi
+
+# if [[ -z $(sudo -u postgres psql -t -c '\du' | cut -d \| -f 1 | grep vagrant) ]]; then
+#   sudo -u postgres createuser vagrant -s
+# fi
+
+# # install pdftk (with libpng12 dependency)
+# wget -q -O /tmp/libpng12.deb https://launchpad.net/ubuntu/+archive/primary/+files/libpng12-0_1.2.54-1ubuntu1_amd64.deb
+# sudo dpkg -i /tmp/libpng12.deb
+# rm /tmp/libpng12.deb
+# wget -q -O /tmp/pdftk-java.deb https://launchpad.net/ubuntu/+archive/primary/+files/pdftk-java_3.0.9-1_all.deb
+# sudo apt --fix-broken install /tmp/pdftk-java.deb -y
+# rm /tmp/pdftk-java.deb
 
